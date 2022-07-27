5cf70661549cab9200495f4afeeebe7c10abe2fc
Kusue X Code <92562821+suna-aquatope@users.noreply.github.com>
2022-07-27 14:43:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use('/api', apirouter)
 
 app.listen(PORT, () => {
     console.log(color("Server running on port " + PORT,'green'))
-    conslole.log("List Apinya" + listkey);
+    console.log("List Apinya" + listkey);
 })
 
 module.exports = app
