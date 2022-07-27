840b5b61f9d610c0a00f458e2ac106bf65709033
Kusue X Code <92562821+suna-aquatope@users.noreply.github.com>
2022-07-27 08:24:30 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://kyu2-api.herokuapp.com/';
+var url = 'https://kyu2-api.herokuapp.com';
 try {
 if(url == 'https://zahirr-web.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
